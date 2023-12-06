4fc2dfff070919e44e064ae0eb5a09a83b8798b0
tica <tim.cassens@gmail.com>
2023-12-06 10:09:31 +0100

    Solve day 6

--- AoC2023/Day6/Day6.cs
+++ AoC2023/Day6/Day6.cs
@@ -12,19 +12,65 @@ namespace AoC2023
     {
         public Day6(): base(6) { }
 
-        public override object SolutionExample1 => throw new NotImplementedException();
-        public override object SolutionPuzzle1 => throw new NotImplementedException();
-        public override object SolutionExample2 => throw new NotImplementedException();
-        public override object SolutionPuzzle2 => throw new NotImplementedException();
+        public override object SolutionExample1 => 288;
+        public override object SolutionPuzzle1 => 800280;
+        public override object SolutionExample2 => 71503L;
+        public override object SolutionPuzzle2 => 45128024L;
 
         protected override object Solve1(string filename)
         {
-            throw new NotImplementedException();
+            var lines = System.IO.File.ReadAllLines(filename).ToList();
+
+            var times = lines[0].Split(' ', StringSplitOptions.RemoveEmptyEntries).Skip(1).Select(int.Parse).ToList();
+            var distances = lines[1].Split(' ', StringSplitOptions.RemoveEmptyEntries).Skip(1).Select(int.Parse).ToList();
+
+            int res = 1;
+
+            for( int i = 0; i < times.Count; ++i)
+            {
+                int num = 0;
+
+                for( int pressTime = 0; pressTime <= times[i]; ++pressTime)
+                {
+                    int remainingTime = times[i] - pressTime;
+
+                    int distance = pressTime * remainingTime;
+
+                    if( distance > distances[i])
+                    {
+                        num += 1;
+                    }
+                }
+
+                res *= num;
+            }
+
+            return res;
         }
 
         protected override object Solve2(string filename)
         {
-            throw new NotImplementedException();
+            var lines = System.IO.File.ReadAllLines(filename).ToList();
+
+            var time = int.Parse(String.Concat(lines[0].Split(' ', StringSplitOptions.RemoveEmptyEntries).Skip(1)));
+            var raceDistance = long.Parse(String.Concat(lines[1].Split(' ', StringSplitOptions.RemoveEmptyEntries).Skip(1)));
+
+            long num = 0;
+
+            for (int pressTime = 0; pressTime <= time; ++pressTime)
+            {
+                long remainingTime = time - pressTime;
+
+                long distance = pressTime * remainingTime;
+
+                if (distance > raceDistance)
+                {
+                    num += 1;
+                }
+            }
+                       
+
+            return num;
         }
     }
 }
