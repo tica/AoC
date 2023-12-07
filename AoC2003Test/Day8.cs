23de15f780d59245210b2d9c328cd4a6bd5251b9
tica <tim.cassens@gmail.com>
2023-12-07 10:44:39 +0100

    Prepare day 8

---
+++ AoC2003Test/Day8.cs
@@ -0,0 +1,12 @@
+using AoC2003Test.Internal;
+
+namespace AoC2003Test
+{
+    [TestClass]
+    public class Day8 : TestDayBase
+    {
+        public Day8() : base(new AoC2023.Day8())
+        {
+        }
+    }
+}
