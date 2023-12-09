5527bf58e1a65f8002d93ffd0f33b7dacb254882
tica <tim.cassens@gmail.com>
2023-12-09 08:56:31 +0100

    Solve day 9

--- AoC2023/Day9/Day9.cs
+++ AoC2023/Day9/Day9.cs
@@ -14,19 +14,79 @@ namespace AoC2023
     {
         public Day9(): base(9) { }
 
-        public override object SolutionExample1 => throw new NotImplementedException();
-        public override object SolutionPuzzle1 => throw new NotImplementedException();
-        public override object SolutionExample2 => throw new NotImplementedException();
-        public override object SolutionPuzzle2 => throw new NotImplementedException();
+        public override object SolutionExample1 => 114L;
+        public override object SolutionPuzzle1 => 2098530125L;
+        public override object SolutionExample2 => 2L;
+        public override object SolutionPuzzle2 => 1016L;
+
+        private static IEnumerable<int> CalcDifferences(IEnumerable<int> numbers)
+        {
+            int? prev = null;
+            foreach (int number in numbers)
+            {
+                if (prev != null)
+                {
+                    yield return number - prev.Value;
+                }
+
+                prev = number;
+            }
+        }
+
+        private static int Extrapolate(List<int> numbers)
+        {
+            var d = CalcDifferences(numbers).ToList();
+
+            if( d.All(x => x == 0) )
+            {
+                return numbers.Last();
+            }
+            else
+            {
+                return Extrapolate(d) + numbers.Last();
+            }
+        }
 
         protected override object Solve1(string filename)
         {
-            throw new NotImplementedException();
+            long sum = 0;
+
+            foreach( var line in System.IO.File.ReadAllLines(filename))
+            {
+                var numbers = line.Split(' ').Select(int.Parse).ToList();
+
+                sum += Extrapolate(numbers);
+            }
+
+            return sum;
+        }
+
+        private static int ExtrapolateLeft(List<int> numbers)
+        {
+            var d = CalcDifferences(numbers).ToList();
+
+            if (d.All(x => x == 0))
+            {
+                return numbers.First();
+            }
+            else
+            {
+                return numbers.First() - ExtrapolateLeft(d);
+            }
         }
 
         protected override object Solve2(string filename)
         {
-            throw new NotImplementedException();
+            long sum = 0;
+
+            foreach (var line in System.IO.File.ReadAllLines(filename))
+            {
+                var numbers = line.Split(' ').Select(int.Parse).ToList();
+
+                sum += ExtrapolateLeft(numbers);
+            }
+
+            return sum;
         }
     }
 }
