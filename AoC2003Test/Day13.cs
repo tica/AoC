739e7009be4e2b2cbf9e888ae7274e2116baf196
tica <tim.cassens@gmail.com>
2023-12-13 18:43:34 +0100

    Prepare day 14

---
+++ AoC2003Test/Day13.cs
@@ -0,0 +1,12 @@
+using AoC2003Test.Internal;
+
+namespace AoC2003Test
+{
+    [TestClass]
+    public class Day13 : TestDayBase
+    {
+        public Day13() : base(new AoC2023.Day13())
+        {
+        }
+    }
+}
---
+++ AoC2003Test/Day14.cs
@@ -0,0 +1,12 @@
+using AoC2003Test.Internal;
+
+namespace AoC2003Test
+{
+    [TestClass]
+    public class Day14 : TestDayBase
+    {
+        public Day14() : base(new AoC2023.Day14())
+        {
+        }
+    }
+}
