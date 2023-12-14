2a15aa746cac75041dafe372e566d667937e88f6
tica <tim.cassens@gmail.com>
2023-12-14 09:30:19 +0100

    Prepare day 15

---
+++ AoC2003Test/Day15.cs
@@ -0,0 +1,12 @@
+using AoC2003Test.Internal;
+
+namespace AoC2003Test
+{
+    [TestClass]
+    public class Day15 : TestDayBase
+    {
+        public Day15() : base(new AoC2023.Day15())
+        {
+        }
+    }
+}
