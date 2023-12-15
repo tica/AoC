a9cd9b9e104bc5c2751457be2af2ae4bbcc81fb6
tica <tim.cassens@gmail.com>
2023-12-15 10:20:07 +0100

    Solve day 15

--- AoC2023/Day15/Day15.cs
+++ AoC2023/Day15/Day15.cs
@@ -24,12 +24,101 @@ namespace AoC2023
 
         protected override object Solve1(string filename)
         {
-            throw new NotImplementedException();
+            byte cur = 0;
+            long sum = 0;
+            foreach(var b in System.IO.File.ReadAllBytes(filename).Skip(3))
+            {
+                if( (char)b == ',')
+                {
+                    //Console.WriteLine($" -> {cur}");
+                    sum += cur;
+                    cur = 0;
+                }
+                else
+                {
+                    //Console.Write((char)b);
+                    cur = (byte)((cur + b) * 17);
+                }
+            }
+
+            sum += cur;
+            cur = 0;
+
+            return sum;
+        }
+        
+        private byte Hash(string txt)
+        {
+            byte cur = 0;
+            foreach( var ch in txt)
+            {
+                byte b = (byte)ch;
+                cur = (byte)((cur + b) * 17);
+            }
+            return cur;
         }
 
         protected override object Solve2(string filename)
         {
-            throw new NotImplementedException();
+            var input = System.IO.File.ReadAllText(filename);
+
+            var boxes = Enumerable.Range(0, 256).Select(_ => new List<(string, int)>()).ToArray();
+
+            foreach( var cmd in input.Split(','))
+            {
+                if( cmd.EndsWith('-') )
+                {
+                    var name = cmd.Substring(0, cmd.Length - 1);
+                    var h = Hash(name);
+
+                    boxes[h].RemoveAll(x => x.Item1 == name);
+                }
+                else
+                {
+                    var m = Regex.Match(cmd, @"^(.+)=(\d+)$");
+                    var name = m.Groups[1].Value;
+                    var value = int.Parse(m.Groups[2].Value);
+
+                    var h = Hash(name);
+
+                    var i = boxes[h].FindIndex(x => x.Item1 == name);
+                    if( i >= 0)
+                    {
+                        boxes[h][i] = (name, value);
+                    }
+                    else
+                    {
+                        boxes[h].Add((name, value));
+                    }
+                }
+
+                /*
+                for( int i = 0; i < 256; ++i )
+                {
+                    if (boxes[i].Count > 0)
+                    {
+                        Console.Write($"Box {i}: ");
+                        foreach( var x in boxes[i])
+                        {
+                            Console.Write($"[{x.Item1} {x.Item2}] ");
+                        }
+                        Console.WriteLine();
+                    }
+                }
+                Console.WriteLine();
+                */
+            }
+
+            long sum = 0;
+            for( int i = 0; i < 256; ++i)
+            {
+                for( int j = 0; j < boxes[i].Count; ++j)
+                {
+                    sum += (i + 1) * (j + 1) * boxes[i][j].Item2;
+                }
+            }
+
+            return sum;
         }
     }
 }
