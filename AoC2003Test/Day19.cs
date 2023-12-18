39a5d167bb5047fe084d52376950e137bf58dc52
tica <tim.cassens@gmail.com>
2023-12-18 15:35:08 +0100

    Prepare day 19

---
+++ AoC2003Test/Day19.cs
@@ -0,0 +1,12 @@
+using AoC2003Test.Internal;
+
+namespace AoC2003Test
+{
+    [TestClass]
+    public class Day19 : TestDayBase
+    {
+        public Day19() : base(new AoC2023.Day19())
+        {
+        }
+    }
+}
---
+++ AoC2023/Day19/Day19.cs
@@ -0,0 +1,33 @@
+﻿using AoC2023.Util;
+using System;
+using System.Diagnostics;
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+using System.Text.RegularExpressions;
+
+using Grid = AoC2023.Util.Grid<char>;
+using Range = AoC2023.Util.Range;
+
+namespace AoC2023
+{
+    public class Day19 : DayBase
+    {
+        public Day19(): base(19) { }
+
+        public override object SolutionExample1 => throw new NotImplementedException();
+        public override object SolutionPuzzle1 => throw new NotImplementedException();
+        public override object SolutionExample2 => throw new NotImplementedException();
+        public override object SolutionPuzzle2 => throw new NotImplementedException();
+
+        protected override object Solve1(string filename)
+        {
+            throw new NotImplementedException();
+        }
+
+        protected override object Solve2(string filename)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
