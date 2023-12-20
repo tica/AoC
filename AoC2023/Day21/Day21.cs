ea5cfbc28f2fe0f0c008d21953874fb56535c40c
tica <tim.cassens@gmail.com>
2023-12-20 11:58:43 +0100

    Prepare day 21

---
+++ AoC2023/Day21/Day21.cs
@@ -0,0 +1,33 @@
+﻿using AoC2023.Util;
+using System;
+using System.Diagnostics;
+using System.Net.WebSockets;
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+using System.Text.RegularExpressions;
+
+using Range = AoC2023.Util.Range;
+
+namespace AoC2023
+{
+    public class Day21 : DayBase
+    {
+        public Day21(): base(21) { }
+
+        public override object SolutionExample1 => throw new NotImplementedException();
+        public override object SolutionPuzzle1 => throw new NotImplementedException();
+        public override object SolutionExample2 => throw new NotImplementedException();
+        public override object SolutionPuzzle2 => throw new NotImplementedException();
+
+        protected override object Solve1(string filename)
+        {
+            throw new NotImplementedException();
+        }
+
+        protected override object Solve2(string filename)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
