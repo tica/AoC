552bbba06c7c185589c8ed4fac4276b9fc820ec6
tica <tim.cassens@gmail.com>
2023-12-23 07:46:17 +0100

    Prepare day 24

---
+++ AoC2003Test/Day24.cs
@@ -0,0 +1,12 @@
+using AoC2003Test.Internal;
+
+namespace AoC2003Test
+{
+    [TestClass]
+    public class Day24 : TestDayBase
+    {
+        public Day24() : base(new AoC2023.Day24())
+        {
+        }
+    }
+}
---
+++ AoC2023/Day24/Day24.cs
@@ -0,0 +1,39 @@
+﻿using AoC2023.Util;
+using System;
+using System.Diagnostics;
+using System.Net.WebSockets;
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+using System.Text.RegularExpressions;
+using Grid = AoC2023.Util.Grid<char>;
+using Coord = AoC2023.Util.Grid<char>.Coord;
+using Range = AoC2023.Util.Range;
+using System.IO;
+using System.Text;
+using System.ComponentModel.Design;
+using System.Runtime.Intrinsics.Arm;
+using System.Xml.Serialization;
+
+namespace AoC2023
+{
+    public class Day24 : DayBase
+    {
+        public Day24(): base(24) { }
+
+        public override object SolutionExample1 => throw new NotImplementedException();
+        public override object SolutionPuzzle1 => throw new NotImplementedException();
+        public override object SolutionExample2 => throw new NotImplementedException();
+        public override object SolutionPuzzle2 => throw new NotImplementedException();
+
+        protected override object Solve1(string filename)
+        {
+            throw new NotImplementedException();
+        }
+
+        protected override object Solve2(string filename)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
