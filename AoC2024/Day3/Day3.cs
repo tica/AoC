2bae20ac5f527c9462e7743491c9d04d2606e835
tica <tim.cassens@gmail.com>
2024-12-03 09:02:06 +0100

    Solve 2024/3

--- AoC2024/Day3/Day3.cs
+++ AoC2024/Day3/Day3.cs
@@ -4,6 +4,7 @@ using System.Diagnostics.CodeAnalysis;
 using System.Linq;
 using System.Security.AccessControl;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 using System.Xml.Schema;
 using static System.Net.Mime.MediaTypeNames;
@@ -14,17 +15,55 @@ namespace AoC2024
     {
         protected override object Solve1(string filename)
         {
-            return null!;
+            var input = System.IO.File.ReadAllText(filename);
+
+            int sum = 0;
+
+            foreach(Match m in Regex.Matches(input, @"mul\((\d\d?\d?),(\d\d?\d?)\)"))
+            {
+                int a = int.Parse(m.Groups[1].Value);
+                int b = int.Parse(m.Groups[2].Value);
+
+                sum += a * b;
+            }
+
+            return sum;
         }
 
         protected override object Solve2(string filename)
         {
-            return null!;
+            var input = System.IO.File.ReadAllText(filename);
+
+            int sum = 0;
+
+            bool enable = true;
+
+            foreach (Match m in Regex.Matches(input, @"mul\((\d\d?\d?),(\d\d?\d?)\)|(do\(\))|(don't\(\))"))
+            {
+                var cmd = m.Groups[0].Value;
+                if (cmd.StartsWith("mul(") && enable)
+                {
+                    int a = int.Parse(m.Groups[1].Value);
+                    int b = int.Parse(m.Groups[2].Value);
+
+                    sum += a * b;
+                }
+                else if (cmd == "do()")
+                {
+                    enable = true;
+                }
+                else if (cmd == "don't()")
+                {
+                    enable = false;
+                }
+            }
+
+            return sum;
         }
 
-        public override object SolutionExample1 => null!;
-        public override object SolutionPuzzle1 => null!;
-        public override object SolutionExample2 => null!;
-        public override object SolutionPuzzle2 => null!;
+        public override object SolutionExample1 => 161;
+        public override object SolutionPuzzle1 => 166630675;
+        public override object SolutionExample2 => 48;
+        public override object SolutionPuzzle2 => 93465710;
     }
 }
