d5edc63ac092d8cd8212bd463f5ca65ad9e48e1a
tica <tim.cassens@gmail.com>
2024-12-03 09:05:39 +0100

    Prepare day 4

---
+++ AoC2024/Day0_template/Day0.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using System.Linq;
+using System.Security.AccessControl;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Schema;
+using static System.Net.Mime.MediaTypeNames;
+
+namespace AoC2024
+{
+    public class Day0 : AoC.DayBase
+    {
+        protected override object Solve1(string filename)
+        {
+            return null!;
+        }
+
+        protected override object Solve2(string filename)
+        {
+            return null!;
+        }
+
+        public override object SolutionExample1 => null!;
+        public override object SolutionPuzzle1 => null!;
+        public override object SolutionExample2 => null!;
+        public override object SolutionPuzzle2 => null!;
+    }
+}
