5ea669479838b761cde364993fbcf499c05fd1a4
tica <tim.cassens@gmail.com>
2024-12-04 09:13:58 +0100

    Prepare 2024/5

---
+++ AoC2024/Day05/Day5.cs
@@ -0,0 +1,21 @@
+﻿
+namespace AoC2024
+{
+    public class Day5 : AoC.DayBase
+    {
+        protected override object Solve1(string filename)
+        {
+            return null!;
+        }
+
+        protected override object Solve2(string filename)
+        {
+            return null!;
+        }
+
+        public override object SolutionExample1 => null!;
+        public override object SolutionPuzzle1 => null!;
+        public override object SolutionExample2 => null!;
+        public override object SolutionPuzzle2 => null!;
+    }
+}
