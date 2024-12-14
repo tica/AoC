81367df12cb23ac6ffb5b2b17659d730ffcfd514
tica <tim.cassens@gmail.com>
2024-12-14 14:00:25 +0100

    Solve 2015/3

---
+++ AoC2015/Day03/Day3.cs
@@ -0,0 +1,71 @@
+﻿using AoC.Util;
+
+namespace AoC2015
+{
+    public class Day3 : AoC.DayBase
+    {
+        Direction ParseDirection(char ch)
+        {
+            return ch switch
+            {
+                '<' => Direction.Left,
+                '>' => Direction.Right,
+                '^' => Direction.Up,
+                'v' => Direction.Down,
+                _ => throw new NotSupportedException()
+            };
+        }
+
+        (int x, int y) Move((int x, int y) p, Direction dir)
+        {
+            return dir switch
+            {
+                Direction.Left => (p.x - 1, p.y),
+                Direction.Right => (p.x + 1, p.y),
+                Direction.Up => (p.x, p.y - 1),
+                Direction.Down => (p.x, p.y + 1),
+                _ => throw new NotSupportedException()
+            };
+        }
+
+        protected override object Solve1(string filename)
+        {
+            var input = File.ReadAllText(filename).Select(ParseDirection);
+
+            var p = (0, 0);
+            var visited = new HashSet<(int, int)>([p]);
+
+            foreach ( var dir in input )
+            {
+                p = Move(p, dir);
+                visited.Add(p);
+            }
+
+            return visited.Count;
+        }
+
+        protected override object Solve2(string filename)
+        {
+            var input = File.ReadAllText(filename).Select(ParseDirection);
+
+            var p = (0, 0);
+            var q = p;
+            var visited = new HashSet<(int, int)>([p]);
+
+            foreach (var dirs in input.Chunk(2))
+            {
+                p = Move(p, dirs.First());
+                visited.Add(p);
+                q = Move(q, dirs.Last());
+                visited.Add(q);
+            }
+
+            return visited.Count;
+        }
+
+        public override object SolutionExample1 => 4;
+        public override object SolutionPuzzle1 => 2592;
+        public override object SolutionExample2 => 3;
+        public override object SolutionPuzzle2 => 2360;
+    }
+}
