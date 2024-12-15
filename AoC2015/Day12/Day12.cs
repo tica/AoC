915fba4d9f25e57484f1417be50f259b0b7b38ca
tica <tim.cassens@gmail.com>
2024-12-15 15:44:44 +0100

    Solve 2015/12

---
+++ AoC2015/Day12/Day12.cs
@@ -0,0 +1,46 @@
+﻿using System.Text.Json;
+using System.Text.Json.Nodes;
+
+namespace AoC2015
+{
+    public class Day12 : AoC.DayBase
+    {
+        private int SumJson(JsonElement elem, bool ignoreRed = false)
+        {
+            switch( elem.ValueKind )
+            {
+                case JsonValueKind.Object:
+                    if (ignoreRed && elem.EnumerateObject().Any(o => o.Value.ToString() == "red"))
+                        return 0;
+                    return elem.EnumerateObject().Sum(o => SumJson(o.Value, ignoreRed));
+                case JsonValueKind.Array:
+                    return elem.EnumerateArray().Sum(a => SumJson(a, ignoreRed));
+                case JsonValueKind.Number:
+                    return elem.GetInt32();
+                default:
+                    return 0;
+            }
+        }
+
+        protected override object Solve1(string filename)
+        {
+            var text = File.ReadAllText(filename);
+            var doc = JsonDocument.Parse(text);
+
+            return SumJson(doc.RootElement);
+        }
+
+        protected override object Solve2(string filename)
+        {
+            var text = File.ReadAllText(filename);
+            var doc = JsonDocument.Parse(text);
+
+            return SumJson(doc.RootElement, true);
+        }
+
+        public override object SolutionExample1 => null!;
+        public override object SolutionPuzzle1 => null!;
+        public override object SolutionExample2 => null!;
+        public override object SolutionPuzzle2 => null!;
+    }
+}
