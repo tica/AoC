5f6b672ae02aab0db96c67fedb5de6d72e2c4582
tica <tim.cassens@gmail.com>
2024-12-15 16:14:19 +0100

    Solve 2015/13

---
+++ AoC2015/Day13/Day13.cs
@@ -0,0 +1,87 @@
+﻿using AoC.Util;
+using System.Text.RegularExpressions;
+
+namespace AoC2015
+{
+    public class Day13 : AoC.DayBase
+    {
+        record class Rule(string Who, string NextTo, int Change)
+        {
+            public static Rule Parse(string line)
+            {
+                var m = Regex.Match(line, @"^(\w+) would (gain|lose) (\d+) happiness units by sitting next to (\w+).$");
+
+                int change = int.Parse(m.Groups[3].Value) * ((m.Groups[2].Value == "lose") ? -1 : 1);
+
+                return new Rule(m.Groups[1].Value, m.Groups[4].Value, change);
+            }
+        }
+
+        private Dictionary<string, Dictionary<string, int>> ParseInput(string filename)
+        {
+            var changes = File.ReadAllLines(filename).Select(Rule.Parse).ToList();
+
+            var lookup = changes.ToLookup(r => r.Who, r => (r.NextTo, r.Change));
+            var dict = lookup.ToDictionary(g => g.Key, g => g.ToDictionary(s => s.NextTo, s => s.Change));
+
+            return dict;
+        }
+
+        int EvaluateHappiness(List<string> seating, Dictionary<string, Dictionary<string, int>> rules)
+        {
+            int h = 0;
+
+            foreach( var (a, b) in seating.Window2() )
+            {
+                h += rules[a][b];
+                h += rules[b][a];
+            }
+
+            h += rules[seating.First()][seating.Last()];
+            h += rules[seating.Last()][seating.First()];
+
+            return h;
+        }
+
+        int EvalTotalHappiness(Dictionary<string, Dictionary<string, int>> input)
+        {
+            var all = input.Keys.ToList();
+
+            int max = 0;
+
+            foreach (var p in all.GetPermutations(all.Count))
+            {
+                var h = EvaluateHappiness(p.ToList(), input);
+                if (h > max)
+                    max = h;
+            }
+
+            return max;
+        }
+
+        protected override object Solve1(string filename)
+        {
+            var input = ParseInput(filename);
+
+            return EvalTotalHappiness(input);
+        }
+
+        protected override object Solve2(string filename)
+        {
+            var input = ParseInput(filename);
+
+            foreach(var v in input.Values)
+            {
+                v.Add("You", 0);
+            }
+            input.Add("You", input.Keys.ToDictionary(k => k, k => 0));
+
+            return EvalTotalHappiness(input);
+        }
+
+        public override object SolutionExample1 => 330;
+        public override object SolutionPuzzle1 => 618;
+        public override object SolutionExample2 => 286;
+        public override object SolutionPuzzle2 => 601;
+    }
+}
--- Common/Util/EnumerableEx.cs
+++ Common/Util/EnumerableEx.cs
@@ -25,5 +25,13 @@ namespace AoC.Util
                 }
             }
         }
+
+        public static IEnumerable<IEnumerable<T>> GetPermutations<T>(this IEnumerable<T> list, int length)
+        {
+            if (length == 1)
+                return list.Select(t => new[] { t });
+
+            return GetPermutations(list, length - 1).SelectMany(t => list.Where(e => !t.Contains(e)), (t1, t2) => t1.Concat([t2]));
+        }
     }
 }
